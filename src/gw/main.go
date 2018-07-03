8560d7b3caebc66a96d9ab058c3ba8bac52686a8
Ales Mikholap <ales.mikholap@gmail.com>
2018-07-03 11:24:51 +0800

    Calculate factorials to generate cpu load

--- src/gw/main.go
+++ src/gw/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
+	"math/big"
+	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().Unix())
 	port := getEnvInt("PLAYGROUND_PORT", 8080)
+
+	log.Printf("starting server on port %d\n", port)
 
 	http.HandleFunc("/", handleRoot)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
@@ -35,9 +41,23 @@ func getEnvInt(name string, fallback int) int {
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%s %s %s%s\n", r.RemoteAddr, r.Method, r.Host, r.URL)
-	data, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Panic(err)
+
+	x := rand.Int() % 1024
+	fibX := fib(x)
+	data := map[int]*big.Int{x: fibX}
+
+	json.NewEncoder(w).Encode(data)
+}
+
+func fib(n int) *big.Int {
+	a := big.NewInt(1)
+	b := big.NewInt(0)
+	tmp := big.NewInt(0)
+
+	for i := 0; i < n; i++ {
+		tmp = tmp.Add(a, b)
+		a, b = tmp, a
 	}
-	w.Write(data)
+
+	return b
 }
